76ce4e9e960346e371851121e2ec9962f42c4cb5
unknown <birajjyo2@gmail.com>
2025-01-21 00:39:55 +0530

    first commitsecond

--- config/mongooes.js
+++ config/mongooes.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://0.0.0.0:27017/taskmaneger');
-// const DB = 'mongodb+srv://birajjyo2:uhnekDGnu6zglcFo@cluster0.kybvwzo.mongodb.net/taskmaneger';
+// mongoose.connect('mongodb://0.0.0.0:27017/taskmaneger');
+const DB =
+  'mongodb+srv://birajjyo2:uhnekDGnu6zglcFo@cluster0.kybvwzo.mongodb.net/taskmaneger';
 
-// mongoose.connect(DB, {
-// 	useNewUrlParser: true,
-// 	useUnifiedTopology: true,
-// });
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 // error
